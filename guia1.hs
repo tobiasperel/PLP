c4136b862bcd55a7e0daea8383cd0a529ede3f17
tobiasperel <peretobias@gmail.com>
2024-08-26 23:33:06 -0300

    cmabios

--- guia1.hs
+++ guia1.hs
@@ -9,5 +9,9 @@ foldr2 f z (x:xs) = f x (foldr2 f z xs)
 sum2 :: Int -> Int  -> Int
 sum2 x y = foldr2 (+) 0 [x,y]
 
+iguales :: Eq a => a -> a -> Bool
+iguales x y =  if x == y then True else False 
+
 elem2 :: a -> [a] -> Bool
 elem2 a [] = False
+elem2 a (x:xs) = foldr2 (iguales) False xs || elem2 a xs
